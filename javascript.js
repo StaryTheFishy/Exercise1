dc549708202a84d99c20e50304cd6385f48eb119
Jessica Dai <115382106+StaryTheFishy@users.noreply.github.com>
2023-04-12 16:10:43 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.write ( "<h1>W3Schools</h1>" );
+document.write ("<p>W3Schools is a web developer information website, with tutorials and references relating to web development topics such as HTML, CSS, JavaScript and PHP.</p>");
+document.write ('<a href="http://www.w3schools.com/">W3Schools</a>');
+document.write ('<br/>')
+document.write ('<img src="http://www.w3schools.com/images/w3schools.png"></img>')
